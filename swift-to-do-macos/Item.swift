89a4ce652e0b9b66c9179d284e549ab1a9dafd3f
Sarmad Gulzar <sarmadgulzar@icloud.com>
2026-06-09 15:40:58 +0500

    Initial Commit

---
+++ swift-to-do-macos/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  swift-to-do-macos
+//
+//  Created by Sarmad Gulzar on 09/06/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ swift-to-do-macosTests/swift_to_do_macosTests.swift
@@ -0,0 +1,19 @@
+//
+//  swift_to_do_macosTests.swift
+//  swift-to-do-macosTests
+//
+//  Created by Sarmad Gulzar on 09/06/2026.
+//
+
+import Testing
+@testable import swift_to_do_macos
+
+struct swift_to_do_macosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
